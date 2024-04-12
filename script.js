5087453dcdc3895b94c8015ee680d7cb5fe696d3
DibyanshuChauhan <cdivyanshu98@gmail.com>
2024-04-12 16:45:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,276 @@
+var users = [
+    {profilepic:"https://images.unsplash.com/photo-1441123100240-f9f3f77ed41b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    displaypicture:"https://images.unsplash.com/photo-1524502397800-2eeaad7c3fe5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+     pendingMessage: 19,
+      location: "London, England",
+       name: "Sunniva",
+        age: "23",
+         intrests: [{
+            icon: `<i class="ri-paint-fill"></i>`,
+            interest: "Painting"
+         },
+         {
+            icon: `<i class="ri-book-open-fill"></i>`,
+            interest: "Reading"
+         }
+        ],
+         bio: "The best way to my heart is a date at the bookstore.",
+         isfriend: null
+        },
+
+        {profilepic:"https://images.unsplash.com/photo-1500336624523-d727130c3328?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displaypicture:"https://images.unsplash.com/photo-1606752445013-eeb8cc06aabd?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 78,
+         location: "Maui",
+          name: "Tajia",
+           age: "25",
+            intrests: [{
+                icon: `<i class="ri-shake-hands-fill"></i>`,
+                interest: "Looking for a partner in crime to explore the world"
+            },
+            {
+                icon: `<i class="ri-ping-pong-fill"></i>`,
+                interest: "Love to play pingpong"
+            }
+        ],
+            bio: "Two reasons to date me: 1. You’d be the good-looking one. 2. Please.",
+            isfriend: null
+           },
+
+           {profilepic:"https://images.unsplash.com/photo-1469334031218-e382a71b716b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+           displaypicture:"https://images.unsplash.com/photo-1462804993656-fac4ff489837?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+           pendingMessage: 57,
+            location: "Turks & Caicos",
+             name: "Vasanta",
+              age: "20",
+               intrests: [{
+                icon: `<i class="ri-chat-1-fill"></i>`,
+                interest: "Love to chatr with friends"
+               },
+               {
+                icon: `<i class="ri-camera-3-fill"></i>`,
+                interest: "And capture every moment of life."
+               }
+            ],
+               bio: "I’m cultured in that I like imported beers and traveling.",
+               isfriend: null
+              },
+
+              {profilepic:"https://images.unsplash.com/photo-1513055911718-30fb8b9c0081?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+              displaypicture:"https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+              pendingMessage: 45,
+               location: "Costa Rica",
+                name: "Zada",
+                 age: "24",
+                  intrests: [{
+                    icon: `<i class="ri-donut-chart-fill"></i>`,
+                    interest: "Love to journal my learnings and experience with programming."
+                  }],
+                  bio: "As a woman in STEM, I’m really good with numbers. I can show you if you give me yours.",
+                  isfriend: null
+                 },
+
+                 {profilepic:"https://images.unsplash.com/photo-1594176778260-461a4a3ae9cb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    displaypicture:"https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+     pendingMessage: 77,
+      location: "Italy",
+       name: "Irish",
+        age: "27",
+         intrests: [{
+            icon: `<i class="ri-empathize-fill"></i>`,
+            interest: "Love to take self care."
+         }],
+         bio: "Old wine with a new bottle, but a better taste.",
+         isfriend: null
+        },
+
+        {profilepic:"https://images.unsplash.com/photo-1618721405821-80ebc4b63d26?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displaypicture:"https://images.unsplash.com/photo-1618721500316-1294305588ac?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+         pendingMessage: 49,
+          location:"Armenia",
+           name: "Elodie",
+            age: "20",
+             intrests: [{
+                icon: `<i class="ri-mini-program-fill"></i>`,
+                interest: "Love to do programming"
+             }],
+             bio: "If you’re tired of searching for the right match?? You can take a break here.",
+             isfriend: null
+            },
+
+            {profilepic:"https://images.unsplash.com/photo-1544963151-fb47c1a06478?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D",
+        displaypicture:"https://images.unsplash.com/photo-1555617135-8724b69f766c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1618721500316-1294305588ac?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+         pendingMessage: 49,
+          location: "Australia Melbourne",
+           name: "Gemma",
+            age: "21",
+             intrests: [{
+                icon: `<i class="ri-drinks-fill"></i>`,
+                interest: "Love to try new smoothies"
+             }],
+             bio: "I want someone to break my heart so I can focus on my studies instead of falling in love.",
+             isfriend: null
+            },
+
+            {profilepic:"https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            displaypicture:"https://images.unsplash.com/photo-1496440737103-cd596325d314?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+             pendingMessage: 56,
+              location: "Austria",
+               name: "Eliana",
+                age: "23",
+                 intrests: [{
+                    icon: `<i class="ri-video-on-line"></i>`,
+                    interest: "I Love to make other's videos"
+                 }],
+                 bio: "I am looking for a guy who is cool enough to melt down on me because I’m too HOT.",
+                 isfriend: null
+                },
+
+                {profilepic:"https://images.unsplash.com/photo-1612904370981-bc1e031ae40e?q=80&w=1890&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                displaypicture:"https://images.unsplash.com/photo-1612905211244-0a071e551ec6?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                 pendingMessage: 56,
+                  location: "Benin",
+                   name: "Livia",
+                    age: "24",
+                     intrests: [{
+                        icon: `<i class="ri-file-excel-2-fill"></i>`,
+                        interest: "Love to ecel my knowledge and love for others in the world"
+                     }],
+                     bio: "I literally want a shag; why else would I have Tinder and my first picture in a bikini.",
+                     isfriend: null
+                    },
+
+                    {profilepic:"https://images.unsplash.com/photo-1519428163141-d3f71425125a?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    displaypicture:"https://images.unsplash.com/photo-1531469271181-7fea8e535e4f?q=80&w=2003&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                     pendingMessage: 65,
+                      location: "Bahamas",
+                       name: "Francesca",
+                        age: "29",
+                         intrests: [{
+                            icon: `<i class="ri-mental-health-line"></i>`,
+                            interest: "I am more focused on being mentally and physically healthy."
+                         }],
+                         bio: "I'm a passionate and caring person who is always looking for new experiences.",
+                         isfriend: null
+                        }
+]
+
+    function select(elem){
+        return document.querySelector(elem);
+    }
+
+
+let curr = 0;
+let isAnimating = false;
+
+function setData(index){
+   select(".proflimg img").src = users[index].profilepic;
+    select(".badge h5").textContent = users[index].pendingMessage;
+    select(".location h3").textContent = users[index].location;
+    select(".name h1:nth-child(1)").textContent = users[index].name;
+    select(".name h1:nth-child(2)").textContent = users[index].age;
+
+    let clutter = "";
+    users[index].intrests.forEach(function(intrest) {
+        clutter += `<div class="tag flex items-center bg-white/30 px-3 py-1 rounded-full gap-3">
+        ${intrest.icon}<h3 class="text-sm tracking-tight">${intrest.interest}</h3>
+    </div>`
+    })
+
+    select(".tags").innerHTML = clutter;
+
+    select(".bio p").textContent = users[index].bio;
+}
+
+(function setInitial(){
+    select(".maincard img").src = users[curr].displaypicture;
+    select(".incomingcard img").src = users[curr+1]?.displaypicture;
+
+    setData(curr);
+
+    curr = 2;
+  }) ();
+
+  function imageswitch(){
+   if(!isAnimating){
+      isAnimating = true;
+      let tl = gsap.timeline({
+         onComplete: function(){
+            isAnimating = false;
+            let main =  select(".maincard");
+            let incoming =  select(".incomingcard");
+   
+            incoming.classList.remove("z-[2]");
+            incoming.classList.add("z-[3]");
+            incoming.classList.remove("incomingcard");
+   
+            main.classList.remove("z-[3]");
+            main.classList.add("z-[2]");
+            
+            gsap.set(main, {
+               scale: 1,
+               opacity: 1
+            })
+            if(curr === users.length) curr = 0;
+            select(".maincard img").src = users[curr].displaypicture;
+            curr++;
+            main.classList.remove("maincard");
+            incoming.classList.add("maincard");
+            main.classList.add("incomingcard")
+         }
+      });
+   
+      tl.to(".maincard", {
+         scale: 1.1,
+         opacity: 0,
+         ease: Circ,
+         duration: .9
+      }, "a" )
+      .from(".incomingcard", {
+         scale: .9,
+         opacity: 0,
+         ease: Circ,
+         duration: 1.1
+      }, "a" )
+   }
+   
+}
+
+  let deny = select(".deny");
+  let accept = select(".accept");
+
+  deny.addEventListener("click", function(){
+     imageswitch();
+     setData(curr-1);
+     gsap.from(".details .element", {
+      y: "100%",
+      opacity: 0,
+      stagger: .06,
+      ease: Power4.easeInOut,
+      duration: 1.5
+     });
+  })
+
+  accept.addEventListener("click", function(){
+   imageswitch();
+   setData(curr-1);
+   gsap.from(".details .element", {
+    y: "100%",
+    opacity: 0,
+    stagger: .06,
+    ease: Power4.easeInOut,
+    duration: 1.5
+   });
+})
+
+  (function containerCreater(){
+    document.querySelector(".element")
+    .forEach(function(element){
+      let div =  document.createElement("div")
+      div.classList.add(`${element.classList[1]}container`,'overflow-hidden');
+      div.appendChild(element);
+      select(".details").appendChild(div);
+    })
+  }) ();
+
+  
